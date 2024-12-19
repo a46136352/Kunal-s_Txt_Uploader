036c2c83bf7e1696f152a176b556099774a4eb63
a46136352 <a46136352@gmail.com>
2024-12-19 11:46:34 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "27994649"
-api_hash = "a667f9bf86b1e9af6ee9862fee3001e4"
-bot_token = "6767726971:AAG0kQTgbAXaqyd0kd1_tR0vdiAO21J48eQ"
+api_id = "28328736"
+api_hash = "802254a44896baa87f3083b7af36b2e5"
+bot_token = "6755775439:AAGkahjp3xK71u-jG6V0uQUR-xJgqLPt9yw"
